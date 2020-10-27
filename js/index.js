71012fe20df2517a4ebd2bdf5fcd604394cae89b
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-27 15:47:31 -0400

    MVP 4-6

--- js/index.js
+++ js/index.js
@@ -23,6 +23,11 @@ logo_h1.addEventListener('mouseover', () => intro.appendChild(busButton));
 busButton.addEventListener('click', (element) => element.target.style.background = "red");
 
 nav_links.forEach(nav_link => {
+    //4
     nav_link.addEventListener('mouseenter', () => nav_link.style.color = "green");
+    //5
     nav_link.addEventListener('mouseleave', () => nav_link.style.color = "blue");
 });
+
+//6
+document.addEventListener('keydown', () => footer.style.background = "yellow");
