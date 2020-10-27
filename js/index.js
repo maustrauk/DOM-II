f5a823812bd7bc646f93e9da1e70e4da36f52132
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-27 15:24:57 -0400

    Start

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let body = document.querySelector("body");
+let header_nav = body.querySelector(".main-navigation");
+let container = body.querySelector(".container home");
+let footer = body.querySelector(".footer");
+
+let busButton = document.createElement("button");
+busButton.textContent = "Click on IT!";
+
+//1
+body.addEventListener('wheel', () =>{ 
+    body.style.background = "#f9affa";
+    header_nav.style.background = "#f9affa";
+});
+
+//2
