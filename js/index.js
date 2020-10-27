783dcbacb46ba2a2ae1807d407c729691a4ced5e
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-27 15:45:00 -0400

    MVP 1-3

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ let body = document.querySelector("body");
 let header_nav = body.querySelector(".main-navigation");
 let container = body.querySelector(".container home");
 let footer = body.querySelector(".footer");
+let logo_h1 = document.querySelector(".logo-heading");
+let intro = document.querySelector(".intro");
+let nav_links = document.querySelectorAll(".nav-link");
 
 let busButton = document.createElement("button");
 busButton.textContent = "Click on IT!";
@@ -14,3 +17,12 @@ body.addEventListener('wheel', () =>{
 });
 
 //2
+logo_h1.addEventListener('mouseover', () => intro.appendChild(busButton));
+
+//3
+busButton.addEventListener('click', (element) => element.target.style.background = "red");
+
+nav_links.forEach(nav_link => {
+    nav_link.addEventListener('mouseenter', () => nav_link.style.color = "green");
+    nav_link.addEventListener('mouseleave', () => nav_link.style.color = "blue");
+});
