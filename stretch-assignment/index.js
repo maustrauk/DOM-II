cd1598f68e5562f4d4da5b00eeacb7b787535187
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-29 21:02:47 -0400

    Stretch task

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,10 +1,25 @@
-let field = document.querySelector(".blocks");
-let blocks = field.querySelectorAll("div");
+const blocks = document.querySelectorAll('.block');
+const blockArray = [];
+blocks.forEach(( block, i) => {
+    block.setAttribute("id", `block${i}`);
+    blockArray.push(block);
+});
 
-
-blocks.forEach(block => block.addEventListener('click', () => {
-    let newBlock = document.createElement('div');
-    newBlock.className = block.className;
-    block.remove();
-    field.appendChild(newBlock);
-}));
+blockArray.forEach(block => {
+    block.addEventListener('click', element => {
+        const targetBlock = document.querySelector("#block0");
+        const elementClass = element.target.classList.value;
+        const targetClass = targetBlock.classList.value;
+        element.target.classList.value = targetClass;
+        targetBlock.classList = elementClass;
+    });
+    block.addEventListener('mousedown', element => {
+        element.target.classList.add('block--moves');
+    });
+    block.addEventListener('mouseup', element => {
+        element.target.classList.remove('block--moves');
+    });
+    block.addEventListener('mouseleave', element => {
+        element.target.classList.remove('block--moves');
+    });
+});
